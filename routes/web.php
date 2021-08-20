92d102b21a0e324f222cdd13a970aa54713f5abc
vagrant <vagrant@homestead>
2021-08-20 16:12:16 +0000

    debug true

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\TaskController;
 use App\Http\Controllers\FolderController;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
